e86c8f64d5454b3c18c5aae37f05879f6d63c624
AdrianArG28 <143119615+AdrianArG28@users.noreply.github.com>
2026-04-22 12:01:56 -0500

    Delete rutas directory

--- rutas/rutasCapturado.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const enrutador = express.Router();
-const pokemonCapturado = require('../controladores/capturadoControlador');
-
-enrutador.post('/capturar', pokemonCapturado.capturarPokemon);
-enrutador.get('/listar/:usuarioCedula', pokemonCapturado.listarPokemonesUsuario);
-
-module.exports = enrutador;
--- rutas/rutasPokemon.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const enrutador = express.Router();
-const pokemonControlador = require('../controladores/pokemonControlador');
-
-enrutador.get('/listar', pokemonControlador.listarPokemones);
-
-module.exports = enrutador;
